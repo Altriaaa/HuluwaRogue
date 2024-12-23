4d992d31e1dceb2494ab987705fcdaee629818ed
Altria <2631710227@qq.com>
2024-12-23 17:27:06 +0800

    Add execute permission for gradlew

--- lwjgl3/src/test/java/GameLogicTest.java
+++ lwjgl3/src/test/java/GameLogicTest.java
@@ -7,6 +7,8 @@ import io.github.altriaaa.huluwarogue.creatures.Orc;
 import io.github.altriaaa.huluwarogue.lwjgl3.Lwjgl3Launcher;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
 
 
 public class GameLogicTest
@@ -19,6 +21,11 @@ public class GameLogicTest
     @Test
     public void setup()
     {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        System.out.println("GLFW initialized successfully.");
         // 初始化 LWJGL 应用程序
         Lwjgl3ApplicationConfiguration config = Lwjgl3Launcher.getDefaultConfiguration();
 //        config.forceExit = false; // 测试结束时不退出 JVM
